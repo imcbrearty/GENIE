e7609a45e40baba1f01b758b31295273f781859b
Artemii Novoselov <crimeacs@yandex.ru>
2023-08-15 16:13:51 -0700

    functional logic

--- Code/make_initial_files.py
+++ Code/make_initial_files.py
@@ -4,63 +4,72 @@ from obspy.clients.fdsn import Client
 from obspy.core import UTCDateTime
 import pathlib
 
-# Load configuration from YAML
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
+def load_config(file_path: str) -> dict:
+    """Load configuration from a YAML file."""
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
 
-lat_range = config['latitude_range']
-lon_range = config['longitude_range']
-depth_range = config['depth_range']
-t0 = UTCDateTime(config['time_range']['start'])
-tf = UTCDateTime(config['time_range']['end'])
-years = list(range(t0.year, tf.year + 1))
+def setup_region(client: Client, config: dict, t0: UTCDateTime, tf: UTCDateTime):
+    """Set up region based on configuration."""
+    return client.get_stations(
+        starttime=t0,
+        endtime=tf,
+        network=config['network'],
+        station='*',
+        minlatitude=config['latitude_range'][0],
+        maxlatitude=config['latitude_range'][1],
+        minlongitude=config['longitude_range'][0],
+        maxlongitude=config['longitude_range'][1]
+    )[0]
 
-# Set up region based on YAML config
-client = Client('IRIS')
-stations = client.get_stations(
-    starttime=t0,
-    endtime=tf,
-    network=config['network'],
-    station='*',
-    minlatitude=lat_range[0],
-    maxlatitude=lat_range[1],
-    minlongitude=lon_range[0],
-    maxlongitude=lon_range[1])[0]
+def extract_station_data(stations):
+    """Extract station code, location and elevation from the stations list."""
+    stas = [station.code for station in stations]
+    locs = [np.array([station.latitude, station.longitude, station.elevation]).reshape(1, -1) for station in stations]
 
-locs = []
-stas = []
-for i in range(len(stations)):
-    stas.append(stations[i].code)
-    locs.append(np.array([stations[i].latitude, stations[i].longitude, stations[i].elevation]).reshape(1, -1))
+    stas = np.hstack(stas)
+    locs = np.vstack(locs)
 
-stas = np.hstack(stas)
-locs = np.vstack(locs)
-iarg = np.argsort(locs[:, 0])
+    iarg = np.argsort(locs[:, 0])
+    return locs[iarg], stas[iarg]
 
-locs = locs[iarg]
-stas = stas[iarg]
+def save_files(base_path: str, locs, stas, config, years):
+    """Save network, region, and 1D velocity model files."""
+    # Save network file
+    np.savez_compressed(base_path + 'stations.npz', locs=locs, stas=stas)
 
-path_to_file = str(pathlib.Path().absolute())
-path_to_file += '\\' if '\\' in path_to_file else '/'
+    # Save region file
+    np.savez_compressed(
+        base_path + 'region.npz',
+        lat_range=config['latitude_range'],
+        lon_range=config['longitude_range'],
+        depth_range=config['depth_range'],
+        deg_pad=config['degree_padding'],
+        num_grids=config['number_of_grids'],
+        n_spatial_nodes=config['number_of_spatial_nodes'],
+        years=years,
+        load_initial_files=config['load_initial_files'],
+        use_pretrained_model=config['use_pretrained_model']
+    )
 
-# Save network file
-np.savez_compressed(path_to_file + 'stations.npz', locs=locs, stas=stas)
+    # Save 1D velocity model
+    np.savez_compressed(
+        base_path + '1d_velocity_model.npz',
+        Depths=np.array(config['velocity_model']['Depths']),
+        Vp=np.array(config['velocity_model']['Vp']),
+        Vs=np.array(config['velocity_model']['Vs'])
+    )
 
-# Save region file
-np.savez_compressed(
-    path_to_file + 'region.npz',
-    lat_range=lat_range,
-    lon_range=lon_range,
-    depth_range=depth_range,
-    deg_pad=config['degree_padding'],
-    num_grids=config['number_of_grids'],
-    n_spatial_nodes=config['number_of_spatial_nodes'],
-    years=years,
-    load_initial_files=config['load_initial_files'],
-    use_pretrained_model=config['use_pretrained_model'])
+if __name__ == '__main__':
+    config = load_config('config.yaml')
 
-# Save 1D velocity model
-Depths = np.array(config['velocity_model']['Depths'])
-Vp = np.array(config['velocity_model']['Vp'])
-Vs = np.array(config['velocity_model']['Vs'])
-np.savez_compressed(path_to_file + '1d_velocity_model.npz', Depths=Depths, Vp=Vp, Vs=Vs)
+    t0 = UTCDateTime(config['time_range']['start'])
+    tf = UTCDateTime(config['time_range']['end'])
+    years = list(range(t0.year, tf.year + 1))
+
+    client = Client('IRIS')
+    stations = setup_region(client, config, t0, tf)
+    locs, stas = extract_station_data(stations)
+
+    base_path = str(pathlib.Path().absolute()) + ('\\' if '\\' in str(pathlib.Path().absolute()) else '/')
+    save_files(base_path, locs, stas, config, years)
