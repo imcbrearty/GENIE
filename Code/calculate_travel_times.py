7b8862ed949b6f4f402ae6e54c74c4f85058a247
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2023-08-21 21:15:50 -0800

    Update calculate_travel_times.py
    Add train travel time neural network to calculate travel times

--- Code/calculate_travel_times.py
+++ Code/calculate_travel_times.py
@@ -94,7 +94,8 @@ depth_steps = np.arange(depth_steps['min_elevation'], depth_steps['max_elevation
 
 ## Load travel times (train regression model, elsewhere, or, load and "initilize" 1D interpolator method)
 path_to_file = str(pathlib.Path().absolute())
-path_to_file += '\\' if '\\' in path_to_file else '/'
+seperator =  '\\' if '\\' in path_to_file else '/'
+path_to_file += seperator
 
 template_ver = 1
 vel_model_ver = 1
@@ -241,7 +242,363 @@ else:
 
 	Tp_interp, Ts_interp = compute_interpolation_parallel(x1, x2, x3, Tp, Ts, X, ftrns1, num_cores = num_cores)
 
-np.savez_compressed(path_to_file + '/1D_Velocity_Models_Regional/%s_1d_velocity_model_ver_%d.npz'%(name_of_project, n_ver), X = X, locs_ref = locs_ref, Tp_interp = Tp_interp, Ts_interp = Ts_interp, Vp_profile = Vp_profile, Vs_profile = Vs_profile, depth_grid = depth_grid)
+save_dense_travel_time_data = True
+if ((train_travel_time_neural_network == False) + (save_dense_travel_time_data == True)) > 0:
+
+	np.savez_compressed(path_to_file + '1D_Velocity_Models_Regional' + seperator + '%s_1d_velocity_model_ver_%d.npz'%(name_of_project, n_ver), X = X, locs_ref = locs_ref, Tp_interp = Tp_interp, Ts_interp = Ts_interp, Vp_profile = Vp_profile, Vs_profile = Vs_profile, depth_grid = depth_grid)
+
+if train_travel_time_neural_network == True:
+
+	rbest_cuda = torch.Tensor(rbest).to(device)
+	mn_cuda = torch.Tensor(mn).to(device)
+
+	## If optimizing projection coefficients with this option, need 
+	## ftrns1 and ftrns2 to accept torch Tensors instead of numpy arrays
+
+	# use_spherical = False
+	if config['use_spherical'] == True:
+
+		earth_radius = 6371e3
+		ftrns1_diff = lambda x: (rbest_cuda @ (lla2ecef_diff(x, e = 0.0, a = earth_radius, device = device) - mn_cuda).T).T # just subtract mean
+		ftrns2_diff = lambda x: ecef2lla_diff((rbest_cuda.T @ x.T).T + mn_cuda, e = 0.0, a = earth_radius, device = device) # just subtract mean
+	
+	else:
+	
+		earth_radius = 6378137.0
+		ftrns1_diff = lambda x: (rbest_cuda @ (lla2ecef_diff(x, device = device) - mn_cuda).T).T # just subtract mean
+		ftrns2_diff = lambda x: ecef2lla_diff((rbest_cuda.T @ x.T).T + mn_cuda, device = device) # just subtract mean
+
+
+	using_3D = False
+	using_1D = True
+	assert((using_3D + using_1D) == 1)
+
+	X_samples, Tp_samples, Ts_samples, Locs_samples = [], [], [], []
+	X_samples_vald, Tp_samples_vald, Ts_samples_vald, Locs_samples_vald = [], [], [], []
+
+	if using_3D == True:
+
+		print('Check if locs_ref are fixed or span 3D space')
+
+		## For 3D, randomly shift the source and reciever to emulate being in 3D (if using fixed at specific node coordinates, or other
+		## wise accept locs_ref as is)
+
+		## Note: instead of locs_ref could use actually locs locations, and obtain a 3D model
+
+		grab_near_station_samples = True
+		if grab_near_station_samples == True:
+
+			t_scale_sample = 5.0
+			n_per_station = 15000
+
+			for n in range(locs_ref.shape[0]):
+
+				p = 1.0/np.maximum(Tp_interp[:,n], 0.1)
+				isample = np.sort(np.random.choice(len(p), size = n_per_station, p = p/p.sum(), replace = False))
+
+				X_samples.append(X[isample])
+				Tp_samples.append(Tp_interp[isample,n])
+				Ts_samples.append(Ts_interp[isample,n])
+				Locs_samples.append(locs_ref[n,:].reshape(1,-1).repeat(len(isample), axis = 0))
+
+		grab_near_boundaries_samples = True
+		if grab_near_boundaries_samples == True:
+
+			t_scale_sample = 5.0
+			n_per_station = 20000
+
+			for n in range(locs_ref.shape[0]):
+
+				p = 1.0/np.maximum(Tp_interp[:,n].max() - Tp_interp[:,n], 0.1)
+				isample = np.sort(np.random.choice(len(p), size = n_per_station, p = p/p.sum(), replace = False))
+
+				X_samples.append(X[isample])
+				Tp_samples.append(Tp_interp[isample,n])
+				Ts_samples.append(Ts_interp[isample,n])
+				Locs_samples.append(locs_ref[n,:].reshape(1,-1).repeat(len(isample), axis = 0))
+
+		grab_interior_samples = True
+		if grab_interior_samples == True:
+
+			t_scale_sample = 5.0
+			n_per_station = 100000
+
+			for n in range(locs_ref.shape[0]):
+
+				p = 1.0*np.ones(Tp_interp.shape[0]) # /np.maximum(Tp_interp[:,n].max() - Tp_interp[:,n], 0.1)
+				isample = np.sort(np.random.choice(len(p), size = n_per_station, p = p/p.sum(), replace = False))
+				isample_vald = np.random.choice(np.delete(np.arange(len(p)), isample, axis = 0), size = 10000)
+
+				X_samples.append(X[isample])
+				Tp_samples.append(Tp_interp[isample,n])
+				Ts_samples.append(Ts_interp[isample,n])
+				Locs_samples.append(locs_ref[n,:].reshape(1,-1).repeat(len(isample), axis = 0))
+
+				X_samples_vald.append(X[isample_vald])
+				Tp_samples_vald.append(Tp_interp[isample_vald,n])
+				Ts_samples_vald.append(Ts_interp[isample_vald,n])
+				Locs_samples_vald.append(locs_ref[n,:].reshape(1,-1).repeat(len(isample_vald), axis = 0))
+
+	## Currently same as using 3D
+	elif use_1D == True:
+
+		grab_near_station_samples = True
+		if grab_near_station_samples == True:
+
+			t_scale_sample = 5.0
+			n_per_station = 15000
+
+			for n in range(locs_ref.shape[0]):
+
+				p = 1.0/np.maximum(Tp_interp[:,n], 0.1)
+				isample = np.sort(np.random.choice(len(p), size = n_per_station, p = p/p.sum(), replace = False))
+
+				X_samples.append(X[isample])
+				Tp_samples.append(Tp_interp[isample,n])
+				Ts_samples.append(Ts_interp[isample,n])
+				Locs_samples.append(locs_ref[n,:].reshape(1,-1).repeat(len(isample), axis = 0))
+
+		grab_near_boundaries_samples = True
+		if grab_near_boundaries_samples == True:
+
+			t_scale_sample = 5.0
+			n_per_station = 20000
+
+			for n in range(locs_ref.shape[0]):
+
+				p = 1.0/np.maximum(Tp_interp[:,n].max() - Tp_interp[:,n], 0.1)
+				isample = np.sort(np.random.choice(len(p), size = n_per_station, p = p/p.sum(), replace = False))
+
+				X_samples.append(X[isample])
+				Tp_samples.append(Tp_interp[isample,n])
+				Ts_samples.append(Ts_interp[isample,n])
+				Locs_samples.append(locs_ref[n,:].reshape(1,-1).repeat(len(isample), axis = 0))
+
+		grab_interior_samples = True
+		if grab_interior_samples == True:
+
+			t_scale_sample = 5.0
+			n_per_station = 100000
+
+			for n in range(locs_ref.shape[0]):
+
+				p = 1.0*np.ones(Tp_interp.shape[0]) # /np.maximum(Tp_interp[:,n].max() - Tp_interp[:,n], 0.1)
+				isample = np.sort(np.random.choice(len(p), size = n_per_station, p = p/p.sum(), replace = False))
+				isample_vald = np.random.choice(np.delete(np.arange(len(p)), isample, axis = 0), size = 10000)
+
+				X_samples.append(X[isample])
+				Tp_samples.append(Tp_interp[isample,n])
+				Ts_samples.append(Ts_interp[isample,n])
+				Locs_samples.append(locs_ref[n,:].reshape(1,-1).repeat(len(isample), axis = 0))
+
+				X_samples_vald.append(X[isample_vald])
+				Tp_samples_vald.append(Tp_interp[isample_vald,n])
+				Ts_samples_vald.append(Ts_interp[isample_vald,n])
+				Locs_samples_vald.append(locs_ref[n,:].reshape(1,-1).repeat(len(isample_vald), axis = 0))
+
+	# Concatenate training dataset
+	X_samples = np.vstack(X_samples)
+	Tp_samples = np.hstack(Tp_samples)
+	Ts_samples = np.hstack(Ts_samples)
+	Locs_samples = np.vstack(Locs_samples)
+
+	# Concatenate validation dataset
+	X_samples_vald = np.vstack(X_samples_vald)
+	Tp_samples_vald = np.hstack(Tp_samples_vald)
+	Ts_samples_vald = np.hstack(Ts_samples_vald)
+	Locs_samples_vald = np.vstack(Locs_samples_vald)
+
+	n_dataset = len(X_samples)
+	n_dataset_vald = len(X_samples_vald)
+
+	irand_sample = np.random.choice(n_dataset, size = int(0.2*n_dataset))
+	scale_val = np.round(2.0*np.linalg.norm(ftrns1(X_samples[irand_sample]) - ftrns1(Locs_samples[irand_sample]), axis = 1).max())
+	trav_val = np.round(1.25*Ts_samples.max())
+
+	print('Using a single model for both phase types')
+	# Note: training a seperate model for either phase type can be more accurate
+	m = TravelTimes(ftrns1_diff, ftrns2_diff, scale_val = scale_val, trav_val = trav_val, n_phases = 2, device = device).to(device)
+
+	# m_p = TravelTimes(ftrns1_diff, ftrns2_diff, n_phases = 1, device = device).to(device)
+	# m_s = TravelTimes(ftrns1_diff, ftrns2_diff, n_phases = 1, device = device).to(device)
+
+	optimizer = optim.Adam(m.parameters(), lr = 0.001)
+	scheduler = StepLR(optimizer, step_size = 10000, gamma = 0.9)
+	loss_func = nn.MSELoss()
+	# loss_func1 = nn.BCELoss()
+
+	n_batch = 5000
+	n_steps = 20001 # 50000
+	n_ver_save = 1
+
+	assert((using_3D + using_1D) == 1)
+	# if use_3D == True:
+	# 	method = 'direct'
+	# elif use_1D == True:
+	# 	method = 'direct relative'
+
+
+	# moi
+
+	losses = []
+	losses_vald = []
+	vald_steps = 10
+	loss_vald = 0.0
+
+	for i in range(n_steps):
+
+		optimizer.zero_grad()
+
+		isample = np.random.randint(0, high = n_dataset, size = n_batch)
+
+		sta_pos = torch.Tensor(Locs_samples[isample]).to(device)
+		src_pos = torch.Tensor(X_samples[isample]).to(device)
+		travel_times_p = Tp_samples[isample] #
+		travel_times_s = Ts_samples[isample] # = sample_inputs_unweighted(n_batch)
+
+		trgt = torch.Tensor(np.concatenate((travel_times_p.reshape(-1,1), travel_times_s.reshape(-1,1)), axis = 1)).to(device)
+
+		if using_3D == True:
+			out = m.forward_relative_train(sta_pos, src_pos, method = 'direct', p = 0.3)
+
+		elif using_1D == True:
+			out = m.forward_relative(sta_pos, src_pos, method = 'direct')
+
+		loss = loss_func(out/m.tscale, trgt/m.tscale)
+
+		# sta_pos, src_pos, masks = sample_masks_unweighted(n_batch)
+		# out = m.forward_mask_train(torch.Tensor(sta_pos).to(device), torch.Tensor(src_pos).to(device), method = 'direct', p = 0.3)
+		# loss = loss + 0.3*loss_func1(out, torch.Tensor(masks.reshape(-1,1)).to(device))
+
+		loss.backward()
+		optimizer.step()
+		scheduler.step()
+		losses.append(loss.item())
+
+		if np.mod(i, vald_steps) == 0:
+
+			with torch.no_grad():
+				isample = np.random.randint(0, high = n_dataset_vald, size = n_batch)
+
+				sta_pos = torch.Tensor(Locs_samples_vald[isample]).to(device)
+				src_pos = torch.Tensor(X_samples_vald[isample]).to(device)
+				travel_times_p = Tp_samples_vald[isample] #
+				travel_times_s = Ts_samples_vald[isample] # = sample_inputs_unweighted(n_batch)
+
+				trgt = torch.Tensor(np.concatenate((travel_times_p.reshape(-1,1), travel_times_s.reshape(-1,1)), axis = 1)).to(device)
+
+				if using_3D == True:
+					out = m.forward_relative_train(sta_pos, src_pos, method = 'direct', p = 0.3)
+
+				elif using_1D == True:
+					out = m.forward_relative(sta_pos, src_pos, method = 'direct')
+
+			loss_vald = loss_func(out/m.tscale, trgt/m.tscale)
+
+			losses_vald.append(loss_vald.item())
+
+		print('%d %0.8f %0.8f'%(i, loss.item(), loss_vald.item()))
+
+	with torch.no_grad():
+
+		## Mesure loss on training and validation and make residual plot
+
+		isample = np.random.randint(0, high = n_dataset, size = n_batch)
+		sta_pos1 = torch.Tensor(Locs_samples[isample]).to(device)
+		src_pos1 = torch.Tensor(X_samples[isample]).to(device)
+		travel_times_p = Tp_samples[isample] #
+		travel_times_s = Ts_samples[isample] # = sample_inputs_unweighted(n_batch)
+		trgt1 = np.concatenate((travel_times_p.reshape(-1,1), travel_times_s.reshape(-1,1)), axis = 1)
+		if using_3D == True:
+			out1 = m.forward_relative_train(sta_pos1, src_pos1, method = 'direct', p = 0.3).cpu().detach().numpy()
+		elif using_1D == True:
+			out1 = m.forward_relative(sta_pos1, src_pos1, method = 'direct').cpu().detach().numpy()
+
+
+		isample = np.random.randint(0, high = n_dataset_vald, size = n_batch)
+		sta_pos2 = torch.Tensor(Locs_samples_vald[isample]).to(device)
+		src_pos2 = torch.Tensor(X_samples_vald[isample]).to(device)
+		travel_times_p = Tp_samples_vald[isample] #
+		travel_times_s = Ts_samples_vald[isample] # = sample_inputs_unweighted(n_batch)
+		trgt2 = np.concatenate((travel_times_p.reshape(-1,1), travel_times_s.reshape(-1,1)), axis = 1)
+		if using_3D == True:
+			out2 = m.forward_relative_train(sta_pos2, src_pos2, method = 'direct', p = 0.3).cpu().detach().numpy()
+		elif using_1D == True:
+			out2 = m.forward_relative(sta_pos2, src_pos2, method = 'direct').cpu().detach().numpy()
+
+	make_plot = True
+	if make_plot == True:
+
+		# path_to_file = str(pathlib.Path().absolute())
+
+		fig, ax = plt.subplots(1, figsize = [8,5])
+		ax.plot(losses, label = 'Train')
+		ax.plot(vald_steps*np.arange(len(losses_vald)), losses_vald, label = 'Vald')
+		ax.set_yscale('log')
+		ax.legend()
+		fig.savefig(path_to_file + 'Plots' + seperator + 'losses_travel_time_model.png', bbox_inches = 'tight', pad_inches = 0.2)
+
+		fig, ax = plt.subplots(2, 2, figsize = [10,10])
+		ax[0,0].scatter(trgt1[:,0], out1[:,0] - trgt1[:,0], 30, alpha = 0.75, label = 'Train')
+		ax[0,0].scatter(trgt2[:,0], out2[:,0] - trgt2[:,0], 30, alpha = 0.75, label = 'Vald')
+		ax[0,0].set_xlabel('Travel Time (P wave)')
+		ax[0,0].set_ylabel('Travel Time Residual (P wave)')
+		ax[0,0].legend()
+
+		ax[0,1].scatter(trgt1[:,1], out1[:,1] - trgt1[:,1], 30, alpha = 0.75, label = 'Train')
+		ax[0,1].scatter(trgt2[:,1], out2[:,1] - trgt2[:,1], 30, alpha = 0.75, label = 'Vald')
+		ax[0,1].set_xlabel('Travel Time (S wave)')
+		ax[0,1].set_ylabel('Travel Time Residual (S wave)')
+		ax[0,1].legend()
+
+		ax[1,0].hist(out1[:,0] - trgt1[:,0], 30, alpha = 0.75, label = 'Train')
+		ax[1,0].hist(out2[:,0] - trgt2[:,0], 30, alpha = 0.75, label = 'Vald')
+		ax[1,0].set_xlabel('Travel Time Residual (P wave)')
+		ax[1,0].set_ylabel('Counts')
+		ax[1,0].legend()
+
+		ax[1,1].hist(out1[:,1] - trgt1[:,1], 30, alpha = 0.75, label = 'Train')
+		ax[1,1].hist(out2[:,1] - trgt2[:,1], 30, alpha = 0.75, label = 'Vald')
+		ax[1,1].set_xlabel('Travel Time Residual (S wave)')
+		ax[1,1].set_ylabel('Counts')
+		ax[1,1].legend()
+		fig.savefig(path_to_file + 'Plots' + seperator + 'residuals_travel_time_model.png', bbox_inches = 'tight', pad_inches = 0.2)
+
+		fig, ax = plt.subplots(1,2, figsize = [10,5])
+		ax[0].scatter(torch.norm(ftrns1_diff(sta_pos1) - ftrns1_diff(src_pos1), dim = 1).cpu().detach().numpy()/1000.0, trgt1[:,0], alpha = 0.5, label = 'Trgt')
+		ax[0].scatter(torch.norm(ftrns1_diff(sta_pos1) - ftrns1_diff(src_pos1), dim = 1).cpu().detach().numpy()/1000.0, out1[:,0], alpha = 0.5, label = 'Pred')
+		ax[0].set_xlabel('Source - Reciever Distance (km)')
+		ax[0].set_ylabel('Travel Time (P wave)')
+		ax[0].legend()
+
+		ax[1].scatter(torch.norm(ftrns1_diff(sta_pos1) - ftrns1_diff(src_pos1), dim = 1).cpu().detach().numpy()/1000.0, trgt1[:,1], alpha = 0.5, label = 'Trgt')
+		ax[1].scatter(torch.norm(ftrns1_diff(sta_pos1) - ftrns1_diff(src_pos1), dim = 1).cpu().detach().numpy()/1000.0, out1[:,1], alpha = 0.5, label = 'Pred')
+		ax[1].set_xlabel('Source - Reciever Distance (km)')
+		ax[1].set_ylabel('Travel Time (S wave)')
+		ax[1].legend()
+		fig.savefig(path_to_file + 'Plots' + seperator + 'travel_time_vs_distance.png', bbox_inches = 'tight', pad_inches = 0.2)
+
+		fig, ax = plt.subplots(2,1, figsize = [10,5])
+		ones_vec = torch.Tensor([1.0, 1.0, 0.0]).reshape(1,-1).to(device)
+		ax[0].scatter(torch.norm(ftrns1_diff(sta_pos1*ones_vec) - ftrns1_diff(src_pos1*ones_vec), dim = 1).cpu().detach().numpy()/1000.0, src_pos1[:,2].cpu().detach().numpy()/1000.0, c = out1[:,0], label = 'P')
+		ax[1].scatter(torch.norm(ftrns1_diff(sta_pos1*ones_vec) - ftrns1_diff(src_pos1*ones_vec), dim = 1).cpu().detach().numpy()/1000.0, src_pos1[:,2].cpu().detach().numpy()/1000.0, c = out1[:,1], label = 'S')
+		ax[1].set_xlabel('Source - Reciever Distance (km)')
+		ax[0].set_ylabel('Source Depth (km)')
+		ax[1].set_ylabel('Source Depth (km)')
+		ax[0].legend()
+		ax[1].legend()
+		fig.savefig(path_to_file + 'Plots' + seperator + 'travel_time_vs_distance_depth.png', bbox_inches = 'tight', pad_inches = 0.2)
+
+		plt.close('all')
+
+	phase = 'p_s'
+	scale_val = m.scale.cpu().detach().numpy()
+	trav_val = m.tscale.cpu().detach().numpy()
+	m = m.cpu()
+	path_save = path_to_file + '1D_Velocity_Models_Regional' + seperator
+	torch.save(m.state_dict(), path_save + 'travel_time_neural_network_%s_ver_%d.h5'%(phase, n_ver_save))
+	torch.save(optimizer.state_dict(), path_save + 'travel_time_neural_network_%s_optimizer_ver_%d.h5'%(phase, n_ver_save))
+	np.savez_compressed(path_save + 'travel_time_neural_network_%s_losses_ver_%d.npz'%(phase, n_ver_save), out1 = out1, out2 = out2, trgt1 = trgt1, trgt2 = trgt2, sta_pos1 = sta_pos1.cpu().detach().numpy(), src_pos1 = src_pos1.cpu().detach().numpy(), sta_pos2 = sta_pos2.cpu().detach().numpy(), src_pos2 = src_pos2.cpu().detach().numpy(), scale_val = scale_val, trav_val = trav_val, losses = losses, losses_vald = losses_vald)
 
 print("All files saved successfully!")
 print("✔ Script execution: Done")
