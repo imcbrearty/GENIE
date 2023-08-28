67b47a1ae43bb8b4673ecf4ad9a34dd61bc2f6d0
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2023-08-28 15:01:03 -0800

    Update train_GENIE_model.py
    Change default visualize predictions step

--- Code/train_GENIE_model.py
+++ Code/train_GENIE_model.py
@@ -902,7 +902,7 @@ for i in range(n_restart_step, n_epochs):
 		pick_lbls = pick_labels_extract_interior_region(x_src_query_cart, tq_sample.cpu().detach().numpy(), lp_meta[i0][:,-2::], lp_srcs[i0], lat_range_interior, lon_range_interior, ftrns1, sig_t = src_t_arv_kernel, sig_x = src_x_arv_kernel)
 		loss = (weights[0]*loss_func(out[0][:,:,0], torch.Tensor(Lbls[i0]).to(device)) + weights[1]*loss_func(out[1][:,:,0], torch.Tensor(Lbls_query[i0]).to(device)) + weights[2]*loss_func(out[2][:,:,0], pick_lbls[:,:,0]) + weights[3]*loss_func(out[3][:,:,0], pick_lbls[:,:,1]))/n_batch
 
-		n_visualize_step = 0
+		n_visualize_step = 250
 		make_visualize_predictions = False
 		if (make_visualize_predictions == True)*(np.mod(i, n_visualize_step) == 0):
 			save_plots_path = path_to_file + seperator + 'Plots' + seperator
