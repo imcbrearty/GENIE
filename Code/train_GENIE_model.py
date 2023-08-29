2e9868ac02e00dd1e058a9d826d11ecb554aaf74
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2023-08-28 16:07:15 -0800

    Update train_GENIE_model.py
    Change inputs to visualize predictions

--- Code/train_GENIE_model.py
+++ Code/train_GENIE_model.py
@@ -908,9 +908,9 @@ for i in range(n_restart_step, n_epochs):
 			save_plots_path = path_to_file + seperator + 'Plots' + seperator
 
 			if Lbls_query[i0][:,5].max() > 0.2: # Plot all true sources
-				visualize_predictions(i0, save_plots_path, n_step = i)
+				visualize_predictions(out, Lbls_query[i0], pick_lbls, X_query[i0], lp_times[i0], lp_stations[i0], Locs[i0], data, i0, save_plots_path, n_step = i)
 			elif np.random.rand() > 0.8: # Plot a fraction of false sources
-				visualize_predictions(i0, save_plots_path, n_step = i)
+				visualize_predictions(out, Lbls_query[i0], pick_lbls, X_query[i0], lp_times[i0], lp_stations[i0], Locs[i0], data, i0, save_plots_path, n_step = i)
 
 		if i0 != (n_batch - 1):
 			loss.backward(retain_graph = True)
