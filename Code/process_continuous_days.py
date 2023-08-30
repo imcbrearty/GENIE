1be7c8639c1b0cab5db38baa1968c54b3dd1e251
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2023-08-29 21:33:13 -0800

    Update process_continuous_days.py
    Add device input to load travel time neural network

--- Code/process_continuous_days.py
+++ Code/process_continuous_days.py
@@ -225,7 +225,7 @@ if config['train_travel_time_neural_network'] == False:
 elif config['train_travel_time_neural_network'] == True:
 
 	n_ver_trv_time_model_load = 1
-	trv = load_travel_time_neural_network(path_to_file, ftrns1_diff, ftrns2_diff, n_ver_trv_time_model_load)
+	trv = load_travel_time_neural_network(path_to_file, ftrns1_diff, ftrns2_diff, n_ver_trv_time_model_load, device = device)
 
 if (use_differential_evolution_location == False)*(config['train_travel_time_neural_network'] == False):
 	hull = ConvexHull(X)
