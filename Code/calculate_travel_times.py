5ea4ac2258275477c8622b832721969882d71fa7
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2024-04-14 05:34:22 -0800

    Update calculate_travel_times.py

--- Code/calculate_travel_times.py
+++ Code/calculate_travel_times.py
@@ -262,7 +262,7 @@ else:
 
 if ((train_travel_time_neural_network == False) + (save_dense_travel_time_data == True)) > 0:
 
-	np.savez_compressed(path_to_file + '1D_Velocity_Models_Regional' + seperator + '%s_1d_velocity_model_ver_%d.npz'%(name_of_project, n_ver), X = X, locs_ref = locs_ref, Tp_interp = Tp_interp, Ts_interp = Ts_interp, Vp_profile = Vp_profile, Vs_profile = Vs_profile, depth_grid = depth_grid)
+	np.savez_compressed(path_to_file + '1D_Velocity_Models_Regional' + seperator + '%s_1d_velocity_model_ver_%d.npz'%(name_of_project, vel_model_ver), X = X, locs_ref = locs_ref, Tp_interp = Tp_interp, Ts_interp = Ts_interp, Vp_profile = Vp_profile, Vs_profile = Vs_profile, depth_grid = depth_grid)
 
 if train_travel_time_neural_network == True:
 
