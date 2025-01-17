a6445d54668b7d984b85057e32315f371a1419be
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2025-01-17 05:35:48 -0800

    Update train_GENIE_model.py

--- Code/train_GENIE_model.py
+++ Code/train_GENIE_model.py
@@ -838,7 +838,7 @@ if config['train_travel_time_neural_network'] == False:
 
 elif config['train_travel_time_neural_network'] == True:
 
-	n_ver_trv_time_model_load = 1
+	n_ver_trv_time_model_load = vel_model_ver # 1
 	trv = load_travel_time_neural_network(path_to_file, ftrns1_diff, ftrns2_diff, n_ver_trv_time_model_load, use_physics_informed = use_physics_informed, device = device)
 	trv_pairwise = load_travel_time_neural_network(path_to_file, ftrns1_diff, ftrns2_diff, n_ver_trv_time_model_load, method = 'direct', use_physics_informed = use_physics_informed, device = device)
 
