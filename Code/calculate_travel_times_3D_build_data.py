56f78c7fc5630b98b3316fcd4275ca962cdadcda
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2025-01-31 03:52:26 -0800

    Update calculate_travel_times_3D_build_data.py

--- Code/calculate_travel_times_3D_build_data.py
+++ Code/calculate_travel_times_3D_build_data.py
@@ -267,7 +267,7 @@ elif load_model_type == 2:
 		
 		## Add a pertubation to elevation, check if the point is moving further away or closer to the nearest point on the surface
 		tree = cKDTree(ftrns1(xx_surface))
-		inear_surface = np.where(ftrns2(xx)[:,2] >= (0.9*(dist_range[1] - dist_range[0]) + dist_range[0]))[0]
+		inear_surface = np.where(ftrns2(xx)[:,2] >= (0.9*(depth_range[1] - depth_range[0]) + depth_range[0]))[0]
 		unit_out = ftrns1(ftrns2(xx[inear_surface]) + np.concatenate((np.zeros((len(inear_surface),2)), 1000.0*np.ones((len(inear_surface),1))), axis = 1))
 		dist_near = tree.query(xx[inear_surface])[0]
 		dist_perturb = tree.query(unit_out)[0]
