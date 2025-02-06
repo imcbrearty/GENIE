9e99fe4c08766d3c38fda0c2a368c4e4f3e85231
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2025-02-05 23:57:10 -0800

    Update process_continuous_days.py

--- Code/process_continuous_days.py
+++ Code/process_continuous_days.py
@@ -509,6 +509,7 @@ for cnt, strs in enumerate([0]):
 			A_Lg_in_src = Data(x = spatial_vals, edge_index = torch.Tensor(np.ascontiguousarray(np.flip(A_src_in_prod.cpu().detach().numpy(), axis = 0))).long()).to(device)
 			trv_out = trv(torch.Tensor(locs[ind_use]).to(device), torch.Tensor(x_grids[i]).to(device)).detach().reshape(-1,2) ## Can replace trv_out with Trv_out
 			mz_list[i].set_adjacencies(A_prod_sta_sta, A_prod_src_src, A_src_in_edges, A_Lg_in_src, A_src_in_sta, A_src_src, torch.Tensor(A_edges_time_p).long().to(device), torch.Tensor(A_edges_time_s).long().to(device), torch.Tensor(A_edges_ref).to(device), trv_out, torch.Tensor(ftrns1(locs_use)).to(device), torch.Tensor(ftrns1(x_grids[i])).to(device))
+			A_src_in_sta_l.append(A_src_in_sta.cpu().detach().numpy())
 
 		else:
 
