5a45249e50866e480179c803fa71184d00b752b5
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2025-06-05 14:03:54 -0700

    Update process_continuous_days.py

--- Code/process_continuous_days.py
+++ Code/process_continuous_days.py
@@ -436,10 +436,10 @@ if compute_magnitudes == True:
 
 	try:
 		n_mag_ver = 1
-		mags_supp = np.load(path_to_file + seperator + 'Grids' + seperator + 'trained_magnitude_model_ver_%d_supplemental.npz'%n_mag_ver)
+		mags_supp = np.load(path_to_file + 'Grids' + seperator + 'trained_magnitude_model_ver_%d_supplemental.npz'%n_mag_ver)
 		mag_grid, k_grid = mags_supp['mag_grid'], int(mags_supp['k_grid'])
 		mags = Magnitude(torch.Tensor(locs).to(device), torch.Tensor(mag_grid).to(device), ftrns1_diff, ftrns2_diff, k = k_grid, device = device)
-		mags.load_state_dict(torch.load(path_to_file + seperator + 'Grids' + seperator + 'trained_magnitude_model_ver_%d.hdf5'%n_mag_ver, map_location = device))
+		mags.load_state_dict(torch.load(path_to_file + 'Grids' + seperator + 'trained_magnitude_model_ver_%d.h5'%n_mag_ver, map_location = device))
 		loaded_mag_model = True
 		print('Will compute magnitudes since a magnitude model was loaded')
 	
