bdd4a87e9d72c31eb6c3ed15f8a453f085f96892
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2025-08-06 00:47:39 -0700

    Update module.py

--- Code/module.py
+++ Code/module.py
@@ -36,8 +36,8 @@ scale_rel = config['scale_rel'] # 30e3
 ## Removing scale_t and eps as free variables. Instead set proportionally to kernel_sig_t
 # scale_t = config['scale_t'] # 10.0
 # eps = config['eps'] # 15.0
-scale_t = train_config['kernel_sig_t']*2.0
-eps = train_config['kernel_sig_t']*3.0
+scale_t = train_config['kernel_sig_t']*3.0
+eps = train_config['kernel_sig_t']*5.0
 
 # use_updated_model_definition = True
 use_phase_types = config['use_phase_types']
@@ -1573,6 +1573,7 @@ class Magnitude(nn.Module):
 		mag = (log_amp + self.activate(self.epicenter_spatial_coef[phase])*pw_log_dist_zero - self.depth_spatial_coef[phase]*pw_log_dist_depths - bias)/torch.maximum(self.activate(self.mag_coef[phase]), torch.Tensor([1e-12]).to(self.device))
 
 		return mag
+
 
 
 
