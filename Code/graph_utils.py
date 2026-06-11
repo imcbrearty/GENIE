e664bd1bb8121f327d25adcca45c4f7b13d4856a
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2026-06-10 18:14:53 -0700

    Update graph_utils.py

--- Code/graph_utils.py
+++ Code/graph_utils.py
@@ -3990,7 +3990,7 @@ def get_domain_bounds(points_lla, scale=1.05, lat_range = None, lon_range = None
 
 
 
-def build_graphs_domain(m_domain, locs_use, stas_use, scale_domain, deg_padding, number_of_spatial_nodes, k_spc_edges, k_sta_edges, depth_range, ftrns1, ftrns2, use_global = False, assign_based_on_grid = False, max_nodes = 3000, n_trgt_nodes = 200e3, Vc = 3500.0, max_time_shift_range = None, file_index = 0, date = [2000, 1, 1], n_grids = 1, initialize = None, use_paths = False, rbest = None, mn = None, optimize_station_graphs = False, optimize_source_graphs = False, use_domain_approximate = True, use_tuner = True, name_of_project = '', verbose = True, device = 'cpu'):
+def build_graphs_domain(m_domain, locs_use, stas_use, scale_domain, deg_padding, number_of_spatial_nodes, k_spc_edges, k_sta_edges, depth_range, ftrns1, ftrns2, use_global = False, assign_based_on_grid = False, max_nodes = 3000, n_trgt_nodes = 200e3, Vc = 3500.0, max_time_shift_range = None, file_index = 0, date = [2000, 1, 1], n_grids = 1, initialize = None, fixed_domain = None, use_paths = False, rbest = None, mn = None, optimize_station_graphs = False, optimize_source_graphs = False, use_domain_approximate = True, use_tuner = True, name_of_project = '', verbose = True, device = 'cpu'):
 
     if initialize is None: # else: [lat_range, lon_range, ]
         domain = get_domain_bounds(locs_use, scale = scale_domain)
@@ -4000,6 +4000,11 @@ def build_graphs_domain(m_domain, locs_use, stas_use, scale_domain, deg_padding,
     if use_global == True:
         domain['lat_range'] = [-90.0, 90.0]
         domain['lon_range'] = [-180.0, 180.0]
+
+    if fixed_domain is not None:
+        domain['lat_range'] = fixed_domain[0]
+        domain['lon_range'] = fixed_domain[1]
+        
 
     lat_range, lon_range = domain['lat_range'], domain['lon_range']
     locs_cart = ftrns1(locs_use)
@@ -4169,7 +4174,7 @@ def build_graphs_domain(m_domain, locs_use, stas_use, scale_domain, deg_padding,
     else:
 
         ## Call fit domain
-        fit_spatial_domain(locs_use, stas_use, scale_domain, deg_padding, number_of_spatial_nodes, k_spc_edges, k_sta_edges, depth_range, ftrns1, ftrns2, use_global = use_global, max_nodes = max_nodes, n_trgt_nodes = n_trgt_nodes, Vc = Vc, file_index = file_index, date = date, rbest = rbest, mn = mn, domain = domain, max_time_shift_range = max_time_shift_range, n_rand_srcs = 150, quantile_times = 0.35, quantile_times_srcs = 0.5, n_grids = n_grids, use_tuner = use_tuner, initialize = initialize, verbose = verbose, device = device)
+        fit_spatial_domain(locs_use, stas_use, scale_domain, deg_padding, number_of_spatial_nodes, k_spc_edges, k_sta_edges, depth_range, ftrns1, ftrns2, use_global = use_global, max_nodes = max_nodes, n_trgt_nodes = n_trgt_nodes, Vc = Vc, file_index = file_index, date = date, rbest = rbest, mn = mn, domain = domain, max_time_shift_range = max_time_shift_range, n_rand_srcs = 150, quantile_times = 0.35, quantile_times_srcs = 0.5, n_grids = n_grids, use_tuner = use_tuner, initialize = initialize, fixed_domain = fixed_domain, verbose = verbose, device = device)
 
         # if initialize is None:
         #     file_load = 'Domains/domain_parameters_%d_%d_%d_%d_ver_1.npz'%(file_index, date[0], date[1], date[2])
@@ -4504,7 +4509,7 @@ def build_graphs_domain(m_domain, locs_use, stas_use, scale_domain, deg_padding,
 
 
 
-def fit_spatial_domain(locs_use, stas_use, scale_domain, deg_padding, number_of_spatial_nodes, k_spc_edges, k_sta_edges, depth_range, ftrns1, ftrns2, use_global = False, max_nodes = 3000, n_trgt_nodes = 200e3, Vc = 3500.0, file_index = 0, date = [2000, 1, 1], rbest = None, mn = None, domain = None, initialize = None, max_time_shift_range = None, n_rand_srcs = 150, quantile_times = 0.35, quantile_times_srcs = 0.5, extend_ratio = 2.0, use_tuner = True, n_grids = 1, n_tuner_steps = 50, verbose = True, device = 'cpu'):
+def fit_spatial_domain(locs_use, stas_use, scale_domain, deg_padding, number_of_spatial_nodes, k_spc_edges, k_sta_edges, depth_range, ftrns1, ftrns2, use_global = False, max_nodes = 3000, n_trgt_nodes = 200e3, Vc = 3500.0, file_index = 0, date = [2000, 1, 1], rbest = None, mn = None, domain = None, initialize = None, fixed_domain = None, max_time_shift_range = None, n_rand_srcs = 150, quantile_times = 0.35, quantile_times_srcs = 0.5, extend_ratio = 2.0, use_tuner = True, n_grids = 1, n_tuner_steps = 50, verbose = True, device = 'cpu'):
 
     # if domain is None:
     #     domain = get_domain_bounds(locs_use, scale = scale_domain)
@@ -4518,8 +4523,12 @@ def fit_spatial_domain(locs_use, stas_use, scale_domain, deg_padding, number_of_
         domain['lat_range'] = [-90.0, 90.0]
         domain['lon_range'] = [-180.0, 180.0]
         deg_padding = 0.0
-    
 
+    if fixed_domain is not None:
+        domain['lat_range'] = fixed_domain[0]
+        domain['lon_range'] = fixed_domain[1]
+
+    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     ## Temporarily define this for simplicity
     def trv(locs, srcs, Vs = Vc):
