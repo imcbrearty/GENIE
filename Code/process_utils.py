8397350d968e1cb1f83b668867b429655d4796cf
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2026-08-02 21:23:27 -0500

    Update process_utils.py

--- Code/process_utils.py
+++ Code/process_utils.py
@@ -1037,7 +1037,7 @@ class PrecomputedEmbedding:
         self.slope_embed_s = torch.sign(-1.0 * torch.diff(self.embed_s, append=self.embed_s[[-1]] + (self.embed_s[[-1]] - self.embed_s[[-2]]), dim=0))
 
 
-def extract_input_from_data(
+def extract_input_from_data1(
     trv_pairwise, P, t0, ind_use, locs, x_grid, A_src_in_sta,
     precomputed_embedding=None, trv_times=None, max_t=300.0, min_t=0.0, 
     kernel_sig_t=5.0, dt=0.2, batch_grids=False, use_asserts=True, 
