b71d324324a3101bbb337888eac600f969864e16
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2026-08-02 21:45:27 -0500

    Update process_continuous_days.py

--- Code/process_continuous_days.py
+++ Code/process_continuous_days.py
@@ -1353,7 +1353,7 @@ for cnt, strs in enumerate([0]):
 				# 	torch.Tensor(x_grids[x_grid_ind][:, 3].reshape(-1, 1)).to(device),
 				# 	X_query_cart_fine, torch.Tensor(X_query_fine[:, [3]]).to(device)
 				# )
-				out = mz_list[x_grid_ind] = forward_queries(X_query_cart_fine, torch.Tensor(X_query_fine[:, [3]]).to(device))
+				out = mz_list[x_grid_ind] = mz_list[x_grid_ind].forward_queries(X_query_cart_fine, torch.Tensor(X_query_fine[:, [3]]).to(device))
 				out_vals_fine += out[1].reshape(-1).cpu().detach().numpy() / len(x_grid_ind_list_1)
 
 			max_val, iargmax = out_vals_fine.max(), np.argmax(out_vals_fine)
