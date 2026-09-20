3d0a28d6cf5385da35f3fccb6bfaa5a2b6025f05
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2026-09-20 15:21:25 -0500

    Adjust scale_t range in graph_utils_backup.py

--- Code/graph_utils_backup.py
+++ Code/graph_utils_backup.py
@@ -899,7 +899,7 @@ class SamplingTuner:
 		# buffer_scale: multiplier for the nominal spacing
 		self.space = [
 			# Real(1e3, 100e3, prior = 'log-uniform', name='scale_t'),	
-			Real(3e3, 15e3, prior = 'log-uniform', name='scale_t'),	  ## Can increase slightly
+			Real(0.5e3, 500e3, prior = 'log-uniform', name='scale_t'),	  ## Can increase slightly
 			Real(0.5, 3.0, name='depth_boost'),   
 			Real(1.0, 2.5, name='buffer_scale')	# prior='log-uniform',
 		]
