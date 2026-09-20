3fc8d08d9840ccab6a2a16de9dd3786ceb72d8b1
imcbrearty <51331267+imcbrearty@users.noreply.github.com>
2026-09-20 13:35:50 -0500

    Fix variable name from t_obsc to t_obs

--- Code/graph_utils_backup.py
+++ Code/graph_utils_backup.py
@@ -12040,7 +12040,7 @@ def probe_network_sidelobes_geodetic(
 
 		if add_travel_time_noise:
 			noise_vals, _ = generate_travel_time_noise(
-				t_obsc.cpu().detach().numpy(),
+				t_obs.cpu().detach().numpy(),
 				phase_input='P',
 				distribution="laplace",
 				sigma_pick=0.15,
